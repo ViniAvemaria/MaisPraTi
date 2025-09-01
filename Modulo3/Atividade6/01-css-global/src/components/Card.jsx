e1ccaede55cdc97de9b6603f5e742e9946985085
ViniAvemaria <160148129+ViniAvemaria@users.noreply.github.com>
2025-09-01 16:26:37 -0300

    Add skeleton loading animation

--- Modulo3/Atividade6/01-css-global/src/components/Card.jsx
+++ Modulo3/Atividade6/01-css-global/src/components/Card.jsx
@@ -1,7 +1,15 @@
+import { useState, useEffect } from "react";
 import { useCart } from "../contexts/CartContex";
 
 function Card({ product }) {
     const { cartItems, addItem, removeItem } = useCart();
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1500);
+    }, []);
 
     const formatPrice = (value) =>
         new Intl.NumberFormat("pt-BR", {
@@ -19,14 +27,26 @@ function Card({ product }) {
 
     return (
         <div className="product-card">
-            <img className="product-img" src={`./Products/${product.img}`} alt={`${product.name} image`} />
-            <div className="product-info">
-                <p className="product-name">{product.name}</p>
-                <p className="price-tag">{formatPrice(product.price)}</p>
-            </div>
-            <button onClick={handleClick} className="add-btn">
-                {inCart ? "Remove from Cart" : "Add to Cart"}
-            </button>
+            {isLoading ? (
+                <>
+                    <div className="skeleton-card">
+                        <div className="skeleton-image"></div>
+                        <div className="skeleton-text long"></div>
+                        <div className="skeleton-text short"></div>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <img className="product-img" src={`./Products/${product.img}`} alt={`${product.name} image`} />
+                    <div className="product-info">
+                        <p className="product-name">{product.name}</p>
+                        <p className="price-tag">{formatPrice(product.price)}</p>
+                    </div>
+                    <button onClick={handleClick} className="add-btn">
+                        {inCart ? "Remove from Cart" : "Add to Cart"}
+                    </button>
+                </>
+            )}
         </div>
     );
 }
