9cf4134c3112b25af3e15e17d8ff26af6a079d27
ViniAvemaria <160148129+ViniAvemaria@users.noreply.github.com>
2025-09-01 15:27:45 -0300

    Fix product names to have 2 lines and ellipses

--- Modulo3/Atividade6/01-css-global/src/components/Card.jsx
+++ Modulo3/Atividade6/01-css-global/src/components/Card.jsx
@@ -20,8 +20,8 @@ function Card({ product }) {
     return (
         <div className="product-card">
             <img className="product-img" src={`./Products/${product.img}`} alt={`${product.name} image`} />
-            <div>
-                <p>{product.name}</p>
+            <div className="product-info">
+                <p className="product-name">{product.name}</p>
                 <p className="price-tag">{formatPrice(product.price)}</p>
             </div>
             <button onClick={handleClick} className="add-btn">
